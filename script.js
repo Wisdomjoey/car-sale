8e8b999118c5ac86eba7dbeac448cd3de4a66e16
Wisdomjoey <joeboywizz@gmail.com>
2024-09-13 02:04:22 +0100

    update

--- script.js
+++ script.js
@@ -1,209 +1,209 @@
-// const navUl = document.getElementById("nav-links");
+const navUl = document.getElementById("nav-links");
 
-// navUl.addEventListener("mouseenter", () => (navUl.dataset.visible = "true"));
-// navUl.addEventListener("mouseleave", () => (navUl.dataset.visible = "false"));
-// navUl.addEventListener("mousemove", (e) => {
-// 	if (e.target === navUl) {
-// 		navUl.style = `
-//     --pos-x: ${e.offsetX}px;
-//     --pos-y: ${e.offsetY}px;
-//     `;
-// 	} else {
-// 		const rect = navUl.getBoundingClientRect();
-// 		const x = e.clientX - rect.left;
-// 		const y = e.clientY - rect.top;
+navUl.addEventListener("mouseenter", () => (navUl.dataset.visible = "true"));
+navUl.addEventListener("mouseleave", () => (navUl.dataset.visible = "false"));
+navUl.addEventListener("mousemove", (e) => {
+	if (e.target === navUl) {
+		navUl.style = `
+    --pos-x: ${e.offsetX}px;
+    --pos-y: ${e.offsetY}px;
+    `;
+	} else {
+		const rect = navUl.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const y = e.clientY - rect.top;
 
-// 		navUl.style = `
-//   --pos-x: ${x}px;
-//   --pos-y: ${y}px;
-//   `;
-// 	}
-// });
+		navUl.style = `
+  --pos-x: ${x}px;
+  --pos-y: ${y}px;
+  `;
+	}
+});
 
-// const langOptions = document.querySelectorAll("#dropdown-langs li");
+const langOptions = document.querySelectorAll("#dropdown-langs li");
 
-// langOptions.forEach((option) => {
-// 	function handler(e) {
-// 		if (
-// 			e.key === "Enter" ||
-// 			e.key === " " ||
-// 			(e.type === "click" && e.target.name == "lang")
-// 		) {
-// 			const ele = document.getElementById("selected-lang");
+langOptions.forEach((option) => {
+	function handler(e) {
+		if (
+			e.key === "Enter" ||
+			e.key === " " ||
+			(e.type === "click" && e.target.name == "lang")
+		) {
+			const ele = document.getElementById("selected-lang");
 
-// 			ele.innerHTML = e.target.labels[0].innerHTML;
-// 		}
-// 	}
+			ele.innerHTML = e.target.labels[0].innerHTML;
+		}
+	}
 
-// 	option.addEventListener("keyup", handler);
-// 	option.addEventListener("click", handler);
-// });
+	option.addEventListener("keyup", handler);
+	option.addEventListener("click", handler);
+});
 
-// const currencyOptions = document.querySelectorAll("#dropdown-currencies li");
+const currencyOptions = document.querySelectorAll("#dropdown-currencies li");
 
-// currencyOptions.forEach((option) => {
-// 	function handler(e) {
-// 		if (
-// 			e.key === "Enter" ||
-// 			e.key === " " ||
-// 			(e.type === "click" && e.target.name == "currency")
-// 		) {
-// 			const ele = document.getElementById("selected-currency");
+currencyOptions.forEach((option) => {
+	function handler(e) {
+		if (
+			e.key === "Enter" ||
+			e.key === " " ||
+			(e.type === "click" && e.target.name == "currency")
+		) {
+			const ele = document.getElementById("selected-currency");
 
-// 			ele.innerHTML = e.target.labels[0].innerHTML;
-// 		}
-// 	}
+			ele.innerHTML = e.target.labels[0].innerHTML;
+		}
+	}
 
-// 	option.addEventListener("keyup", handler);
-// 	option.addEventListener("click", handler);
-// });
+	option.addEventListener("keyup", handler);
+	option.addEventListener("click", handler);
+});
 
-// const partOptions = document.querySelectorAll("#dropdown-parts li");
+const partOptions = document.querySelectorAll("#dropdown-parts li");
 
-// partOptions.forEach((option) => {
-// 	function handler(e) {
-// 		if (
-// 			e.key === "Enter" ||
-// 			e.key === " " ||
-// 			(e.type === "click" && e.target.name == "part")
-// 		) {
-// 			const ele = document.getElementById("selected-part");
+partOptions.forEach((option) => {
+	function handler(e) {
+		if (
+			e.key === "Enter" ||
+			e.key === " " ||
+			(e.type === "click" && e.target.name == "part")
+		) {
+			const ele = document.getElementById("selected-part");
 
-// 			ele.innerHTML = e.target.labels[0].innerHTML;
-// 		}
-// 	}
+			ele.innerHTML = e.target.labels[0].innerHTML;
+		}
+	}
 
-// 	option.addEventListener("keyup", handler);
-// 	option.addEventListener("click", handler);
-// });
+	option.addEventListener("keyup", handler);
+	option.addEventListener("click", handler);
+});
 
-// // Hero Info Slider
-// let index = 0;
-// const slider = document.getElementById("slider");
-// const indicator = document.getElementById("indicator");
+// Hero Info Slider
+let index = 0;
+const slider = document.getElementById("slider");
+const indicator = document.getElementById("indicator");
 
-// setInterval(() => {
-// 	const children = indicator.children;
+setInterval(() => {
+	const children = indicator.children;
 
-// 	index = index >= 2 ? 0 : index + 1;
-// 	slider.style = `--active-info: ${index};`;
+	index = index >= 2 ? 0 : index + 1;
+	slider.style = `--active-info: ${index};`;
 
-// 	for (let i = 0; i < children.length; i++) {
-// 		const ele = children[i];
+	for (let i = 0; i < children.length; i++) {
+		const ele = children[i];
 
-// 		if (ele.classList.contains("active")) {
-// 			ele.classList.remove("active");
+		if (ele.classList.contains("active")) {
+			ele.classList.remove("active");
 
-// 			break;
-// 		}
-// 	}
+			break;
+		}
+	}
 
-// 	children[index].classList.add("active");
-// }, 6000);
+	children[index].classList.add("active");
+}, 6000);
 
-// // brand display video control
-// const vids = document.querySelectorAll(".brand__video");
+// brand display video control
+const vids = document.querySelectorAll(".brand__video");
 
-// vids.forEach((ele) => ele.addEventListener("mouseover", () => ele.play()));
-// vids.forEach((ele) =>
-// 	ele.addEventListener("mouseleave", () => {
-// 		if (ele.playing) {
-// 			ele.currentTime = 0;
-// 			ele.pause();
-// 		}
-// 	})
-// );
+vids.forEach((ele) => ele.addEventListener("mouseover", () => ele.play()));
+vids.forEach((ele) =>
+	ele.addEventListener("mouseleave", () => {
+		if (ele.playing) {
+			ele.currentTime = 0;
+			ele.pause();
+		}
+	})
+);
 
-// // Product Slider Function
-// const rimC = document.getElementById("rim-control");
-// const coilC = document.getElementById("coil-control");
-// const testimonialC = document.getElementById("testimonial-control");
-// let moves = {
-// 	rim: 0,
-// 	coil: 0,
-// 	testimonial: 0,
-// };
+// Product Slider Function
+const rimC = document.getElementById("rim-control");
+const coilC = document.getElementById("coil-control");
+const testimonialC = document.getElementById("testimonial-control");
+let moves = {
+	rim: 0,
+	coil: 0,
+	testimonial: 0,
+};
 
-// for (let i = 0; i < rimC.children.length; i++) {
-// 	const ele = rimC.children[i];
-// 	ele.addEventListener("click", (e) => moveSlider("rim", e.target.id));
-// }
+for (let i = 0; i < rimC.children.length; i++) {
+	const ele = rimC.children[i];
+	ele.addEventListener("click", (e) => moveSlider("rim", e.target.id));
+}
 
-// for (let i = 0; i < coilC.children.length; i++) {
-// 	const ele = coilC.children[i];
-// 	ele.addEventListener("click", (e) => moveSlider("coil", e.target.id));
-// }
+for (let i = 0; i < coilC.children.length; i++) {
+	const ele = coilC.children[i];
+	ele.addEventListener("click", (e) => moveSlider("coil", e.target.id));
+}
 
-// for (let i = 0; i < testimonialC.children.length; i++) {
-// 	const ele = testimonialC.children[i];
-// 	ele.addEventListener("click", (e) => moveSlider("testimonial", e.target.id));
-// }
+for (let i = 0; i < testimonialC.children.length; i++) {
+	const ele = testimonialC.children[i];
+	ele.addEventListener("click", (e) => moveSlider("testimonial", e.target.id));
+}
 
-// function moveSlider(id, direction) {
-// 	const wrapper = document.getElementById(`${id}-slider-wrapper`);
-// 	const control = document.getElementById(`${id}-control`);
-// 	const slider = document.getElementById(`${id}-slider`);
-// 	const wrapperWidth = wrapper.clientWidth;
-// 	const sliderWidth = slider.scrollWidth;
-// 	const translateWidth = slider.children[0].scrollWidth + 8;
-// 	const widthLeft = sliderWidth - wrapperWidth;
+function moveSlider(id, direction) {
+	const wrapper = document.getElementById(`${id}-slider-wrapper`);
+	const control = document.getElementById(`${id}-control`);
+	const slider = document.getElementById(`${id}-slider`);
+	const wrapperWidth = wrapper.clientWidth;
+	const sliderWidth = slider.scrollWidth;
+	const translateWidth = slider.children[0].scrollWidth + 8;
+	const widthLeft = sliderWidth - wrapperWidth;
 
-// 	if (direction === "left") {
-// 		if (moves[id] <= 0) {
-// 			moves[id] = 0;
-// 			control.children[0].setAttribute("disabled", "");
+	if (direction === "left") {
+		if (moves[id] <= 0) {
+			moves[id] = 0;
+			control.children[0].setAttribute("disabled", "");
 
-// 			return;
-// 		}
-// 		if (moves[id] - translateWidth <= 0)
-// 			control.children[0].setAttribute("disabled", "");
-// 		else control.children[1].removeAttribute("disabled");
+			return;
+		}
+		if (moves[id] - translateWidth <= 0)
+			control.children[0].setAttribute("disabled", "");
+		else control.children[1].removeAttribute("disabled");
 
-// 		moves[id] = moves[id] - translateWidth < 0 ? 0 : moves[id] - translateWidth;
-// 		slider.style.transform = `translateX(-${moves[id]}px)`;
-// 	}
+		moves[id] = moves[id] - translateWidth < 0 ? 0 : moves[id] - translateWidth;
+		slider.style.transform = `translateX(-${moves[id]}px)`;
+	}
 
-// 	if (direction === "right") {
-// 		if (moves[id] >= widthLeft) {
-// 			moves[id] = widthLeft;
-// 			control.children[1].setAttribute("disabled", "");
+	if (direction === "right") {
+		if (moves[id] >= widthLeft) {
+			moves[id] = widthLeft;
+			control.children[1].setAttribute("disabled", "");
 
-// 			return;
-// 		}
-// 		if (moves[id] + translateWidth >= widthLeft)
-// 			control.children[1].setAttribute("disabled", "");
-// 		else control.children[0].removeAttribute("disabled");
+			return;
+		}
+		if (moves[id] + translateWidth >= widthLeft)
+			control.children[1].setAttribute("disabled", "");
+		else control.children[0].removeAttribute("disabled");
 
-// 		moves[id] =
-// 			moves[id] + translateWidth > widthLeft
-// 				? widthLeft
-// 				: moves[id] + translateWidth;
-// 		slider.style.transform = `translateX(-${moves[id]}px)`;
-// 	}
-// }
+		moves[id] =
+			moves[id] + translateWidth > widthLeft
+				? widthLeft
+				: moves[id] + translateWidth;
+		slider.style.transform = `translateX(-${moves[id]}px)`;
+	}
+}
 
-// // Footer cursor animation
-// const footer = document.getElementById("footer-wrapper");
+// Footer cursor animation
+const footer = document.getElementById("footer-wrapper");
 
-// footer.addEventListener("mouseenter", () => (footer.dataset.visible = "true"));
-// footer.addEventListener("mouseleave", () => (footer.dataset.visible = "false"));
-// footer.addEventListener("mousemove", (e) => {
-// 	if (e.target === footer) {
-// 		footer.style = `
-//     --pos-x: ${e.offsetX}px;
-//     --pos-y: ${e.offsetY}px;
-//     `;
-// 	} else {
-// 		const rect = footer.getBoundingClientRect();
-// 		const x = e.clientX - rect.left;
-// 		const y = e.clientY - rect.top;
+footer.addEventListener("mouseenter", () => (footer.dataset.visible = "true"));
+footer.addEventListener("mouseleave", () => (footer.dataset.visible = "false"));
+footer.addEventListener("mousemove", (e) => {
+	if (e.target === footer) {
+		footer.style = `
+    --pos-x: ${e.offsetX}px;
+    --pos-y: ${e.offsetY}px;
+    `;
+	} else {
+		const rect = footer.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const y = e.clientY - rect.top;
 
-// 		footer.style = `
-//   --pos-x: ${x}px;
-//   --pos-y: ${y}px;
-//   `;
-// 	}
-// });
+		footer.style = `
+  --pos-x: ${x}px;
+  --pos-y: ${y}px;
+  `;
+	}
+});
 
 // In-view animations
 const observer = new IntersectionObserver(
